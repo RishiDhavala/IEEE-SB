dc835b61bfeb95b5d639cfd24cc5590f079791d5
Biswajit_2600 <biswajitpanda754@gmail.com>
2022-09-06 00:46:23 +0530

    Trial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-     
+     <h1>Hello World, This is the official site of IEEE VSSUT, Burla Student Branch</h1>
     </div>
   );
 }
